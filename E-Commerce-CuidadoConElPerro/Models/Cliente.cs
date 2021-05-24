d0dcc6e882fefb029b9f014e01ecff8c60afc658
martinGM05 <mtn.gm05@gmail.com>
2021-05-24 11:54:35 -0500

    Reguex

--- E-Commerce-CuidadoConElPerro/Models/Cliente.cs
+++ E-Commerce-CuidadoConElPerro/Models/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,11 +9,16 @@ namespace E_Commerce_CuidadoConElPerro.Models
     public partial class Cliente
     {
         public int IdCliente { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio ")]
         public string NomCliente { get; set; }
+        [StringLength(30, ErrorMessage = "La contraseña solo acepta 30 caracteres")]
         public string Contrasenia { get; set; }
         public byte[] Imagen { get; set; }
+        [Required(ErrorMessage = "El telefono es obligatorio ")]
         public string Telefono { get; set; }
+        [Required(ErrorMessage = "El correo es obligatorio ")]
         public string Correo { get; set; }
+        [Required(ErrorMessage = "El domicilio es obligatorio ")]
         public string Domicilio { get; set; }
     }
 }
--- E-Commerce-CuidadoConElPerro/Models/Empleado.cs
+++ E-Commerce-CuidadoConElPerro/Models/Empleado.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,8 +9,11 @@ namespace E_Commerce_CuidadoConElPerro.Models
     public partial class Empleado
     {
         public int IdEmpleado { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio ")]
         public string NombreEmpleado { get; set; }
+        [Required(ErrorMessage = "El usuario es obligatorio ")]
         public string Usuario { get; set; }
+        [StringLength(6, ErrorMessage = "La contraseña solo acepta 6 caracteres")]
         public string Contrasenia { get; set; }
         public byte[] Imagen { get; set; }
         public int? IdRol { get; set; }
